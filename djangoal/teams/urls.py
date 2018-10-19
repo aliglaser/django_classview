286e0c4187208ae2318d327dde8e712cabf36585
Natalie Glaser <nurinuriprettynuri@gmail.com>
2018-10-18 23:29:13 -0700

    finally done
    making mixins is hard....

--- djangoal/teams/urls.py
+++ djangoal/teams/urls.py
@@ -7,5 +7,8 @@ app_name='teams'
 
 urlpatterns = [
     path('', views.TeamListView.as_view(), name='list'),
+    path('create/', views.TeamCreateView.as_view(), name='create'),
     path('<pk>/', views.TeamDetailView.as_view(), name='detail'),
+    path('edit/<pk>/', views.TeamUpdateView.as_view(), name='update'),
+    path('delete/<pk>/', views.TeamDeleteView.as_view(), name='delete'),
 ]
--- djangoal/teams/views.py
+++ djangoal/teams/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from . import models
+from . import mixins
 
 
 def team_list(request):
@@ -15,11 +18,47 @@ def team_detail(request, pk):
 
 
 
-class TeamListView(ListView):
+class TeamListView(CreateView, ListView):
 	model = models.Team
 	context_object_name = 'teams'
+	fields = ('name', 'coach', 'practice_location')
+	template_name = 'teams/team_list.html'
 
 
 
-class TeamDetailView(DetailView):
+class TeamDetailView(DetailView, UpdateView):
+	fields = ('name', 'coach', 'practice_location')
 	model = models.Team	
+	template_name = 'teams/team_detail.html'
+
+
+
+class TeamCreateView(LoginRequiredMixin, mixins.PageTitleMixin, CreateView):
+	model = models.Team
+	fields = ('name', 'coach', 'practice_location')
+
+	page_title = "Create a new team!"
+
+	def get_initial(self):
+		initial = super().get_initial()
+		initial['coach'] = self.request.user.pk
+		return initial
+
+
+class TeamUpdateView(LoginRequiredMixin, mixins.PageTitleMixin, UpdateView):
+	model = models.Team
+	fields = ('name', 'coach', 'practice_location')
+
+	def get_page_title(self):
+		obj = self.get_object()
+		return "Update {}".format(obj.name)
+
+
+class TeamDeleteView(LoginRequiredMixin, DeleteView):
+	model = models.Team
+	success_url = reverse_lazy('teams:list')
+
+	def get_queryset(self):
+		if not self.request.user.is_superuser:
+			return self.model.objects.filter(coach=self.request.user)
+		return self.model.objects.all()	
